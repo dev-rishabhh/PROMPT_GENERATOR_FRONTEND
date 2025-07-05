da79e4001d684bc75484f8a3c359b3c9efe9ca65
Rishabh Pandey <rishabhpandey2963@gmail.com>
2025-07-05 19:35:11 +0530

    fixed bug

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-export const BASE_URL="https://prompt-generator-backend-kj66.onrender.com"
-=======
-export const BASE_URL="https://prompt-generator-backend-kj66.onrender.com"
->>>>>>> 2d7b9d24b85d5b4c96cad17398af3fe2d2b6d372
+export const BASE_URL="https://prompt-generator-backend-kj66.onrender.com/"
+
